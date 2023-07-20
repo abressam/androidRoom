6a15cb20c47dfc2f0f1b55300a6b52124e1dd686
abressam <amandabressam@gmail.com>
2023-07-19 22:46:24 -0300

    Add comments in converters

--- app/src/main/java/br/com/alura/orgs/database/converter/Converters.kt
+++ app/src/main/java/br/com/alura/orgs/database/converter/Converters.kt
@@ -6,10 +6,10 @@ import java.math.BigDecimal
 class Converters {
     @TypeConverter
     fun deDouble(valor: Double?) : BigDecimal {
-        return valor?.let { BigDecimal(valor.toString()) } ?: BigDecimal.ZERO
+        return valor?.let { BigDecimal(valor.toString()) } ?: BigDecimal.ZERO // recebe double e retorna bigDecimal
     }
     @TypeConverter
     fun bigDecimalParaDouble(valor: BigDecimal?) : Double? {
-        return valor?.let { valor.toDouble() }
+        return valor?.let { valor.toDouble() } // recebe bigDecimal e retorna double
     }
 }
