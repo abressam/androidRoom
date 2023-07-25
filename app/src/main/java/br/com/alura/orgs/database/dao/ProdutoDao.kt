eed84449a6e48c5bdaf9d55aa5994284f14572ee
abressam <amandabressam@gmail.com>
2023-07-25 08:22:38 -0300

    Add fun remove and buscaPorId

--- app/src/main/java/br/com/alura/orgs/database/dao/ProdutoDao.kt
+++ app/src/main/java/br/com/alura/orgs/database/dao/ProdutoDao.kt
@@ -1,8 +1,11 @@
 package br.com.alura.orgs.database.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import br.com.alura.orgs.model.Produto
 
 @Dao
@@ -11,7 +14,13 @@ interface ProdutoDao {
     @Query("SELECT * FROM Produto")
     fun buscaTodos() : List<Produto>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun salva(vararg produto: Produto)
+
+    @Delete
+    fun remove(vararg produto: Produto)
+
+    @Query("SELECT * FROM Produto WHERE id = :id")
+    abstract fun buscaPorId(id: Long) : Produto?
 
 }
